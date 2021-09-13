4485e3e623939860dd9546728d3d55e55b232b86
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-09-13 01:23:55 -0300

    Correção no apontamento do backend

--- src/connections.js
+++ src/connections.js
@@ -1,5 +1,6 @@
 // Host de conexão da API
 module.exports = {
   
-  hostServer: process.env.PORT !== undefined ? 'https://rede-social-backend-000.herokuapp.com' : 'http://127.0.0.1:3000'
+  hostServer: 'https://rede-social-backend-000.herokuapp.com'
+  //hostServer: 'http://127.0.0.1:3000'
 }
