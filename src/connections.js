872f208c51aadd1844cecce58d96982e3b833179
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-09-13 18:41:46 -0300

    Ajuste de apontamentos para separar ambiente de testes com ambiente de produção

--- src/connections.js
+++ src/connections.js
@@ -1,6 +1,6 @@
- 
+require('dotenv/config')
+
 // Host de conexão da API
 module.exports = {
-  //hostServer: 'https://rede-social-backend-000.herokuapp.com'
-  hostServer: 'http://127.0.0.1:3000'
+  hostServer: process.env.HOST_BACKEND
 }
